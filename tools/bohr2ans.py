5e41916f4b9f6303509184b1e0d648955d198e71
TITAN <52470545+TITAN-code@users.noreply.github.com>
2019-07-14 19:51:51 +0300

    Add files via upload

--- tools/bohr2ans.py
+++ tools/bohr2ans.py
@@ -1,82 +1,94 @@
 #!/usr/bin/python
 # calc_EFF_ans.py
 # Author: Jing Huang; Date: July 15, 2016
-#
-# A TOOL TO CALCULATE THE STRENGTH OF ELECTRIC FIELD 
-# AT THE POINT (XP, YP, ZP) ORIENTED TO THE DIRECTION 
-# VECTOR   V   FROM THE POINT (V1X, V1Y, V1Z) TO THE 
-# POINT (V2X, V2Y, V2Z)
-#
-import math
-#
-# Define the constant of EF calculation.
-#
-#--------------
-#    INPUT
-#--------------
-#
-NAME = "full"
-#--------------
-# END OF INPUT
-#--------------
-#
-# READ COORDINATIONS AND CHARGES 
-#
-result=[]
-with open(NAME+".txt","r") as f:
-   for line in f:
-      result.append(list(map(float,line.split())))
-      if not line.split():
-	   continue
-#   print(result[1][2])
-f.close()
+
+import math,sys
+
+def header_command_line():
+    print ("                                                                 ")
+    print ("  ****************************************************************** ")
+    print ("  *                                                                * ")
+    print ("  *                    TITAN - VERSION 1.1                         * ")
+    print ("  *                                                                * ")
+    print ("  *           FROM THE HEBREW UNIVERISTY OF JERUSALEM              * ")
+    print ("  *    JING HUANG, THIJS STUYVER, DIBYENDU MALLICK, SASON SHAIK    * ")
+    print ("  *              J. COMPUT. CHEM. X, XXX-XXX (2019)                * ")
+    print ("  *                                                                * ")
+    print ("  ****************************************************************** ")
+    print ("                                                                 ")
+    print ("                                                                 ")
+    print ("                   BOHR TO ANGSTROM CONVERSION                    ")
+    print ("                                                                 ")
+
+def read_input(NAME):
+    result=[]
+    with open(NAME,"r") as f:
+        for line in f:
+            result.append(list(map(float,line.split())))
+            if not line.split():
+                continue
+    f.close()
+    return result
 #
 # result[i][j] : i -> the (i+1) th charge
-# X[i]  = result[i][0]  BOHR 
-# Y[i]  = result[i][1]  BOHR 
-# Z[i]  = result[i][2]  BOHR 
+# X[i]  = result[i][0]  ANGSTROM 
+# Y[i]  = result[i][1]  ANGSTROM 
+# Z[i]  = result[i][2]  ANGSTROM 
 # Q[i]  = result[i][3]  e-        a.u.
 #
 # THE NUMBERS OF THE LINES IN THE FILE
 #
-def linecount1():
- count1 = 0
- count = len(open(NAME+".txt", "rU").readlines())
- with open(NAME+".txt","r") as f:
-   for line in f:
-      if not line.split():
-       count1 = count1 + 1
- f.close()
- line1 = count - count1 
- return line1
-#
-#
-# MAIN PORGRAM
-#
-i = 0
-# NUMBER OF CHARGES
-NO_CHA = linecount1()
-list = range(NO_CHA)
-#
-X = range(NO_CHA)
-Y = range(NO_CHA)
-Z = range(NO_CHA)
-Q = range(NO_CHA)
-#
-f1 = open(NAME+"_ans.txt", "w")
-for i in list:
-#
-    X[i]  = result[i][0]*0.529177249  # X coordinate Bohr 
-    Y[i]  = result[i][1]*0.529177249  # Y coordinate Bohr 
-    Z[i]  = result[i][2]*0.529177249  # Z coordinate Bohr 
-    Q[i]  = result[i][3]  # Charge
-    print >> f1, (" %.10f     %.10f     %.10f     %.10f " %(X[i],Y[i],Z[i],Q[i]))
-#
-print " "
-print "****************"
-print " "
-print "      Done"
-print " "
-print "****************"
-print " "
-print " "
+def linecount1(NAME):
+    count1 = 0
+    count = len(open(NAME, "rU").readlines())
+    with open(NAME,"r") as f:
+        for line in f:
+            if not line.split():
+                count1 = count1 + 1
+    f.close()
+    line1 = count - count1
+    return line1
+
+def convert(NAME):
+    """ the main program """
+    header_command_line()
+    i = 0
+    # NUMBER OF CHARGES
+    result = read_input(NAME)
+    NO_CHA = linecount1(NAME)
+    list = range(NO_CHA)
+    #
+    X = range(NO_CHA)
+    Y = range(NO_CHA)
+    Z = range(NO_CHA)
+    Q = range(NO_CHA)
+    #
+    f1 = open(NAME.strip(".txt")+"_ans.txt", "w")
+    for i in list:
+    #
+        X[i]  = result[i][0]*0.529177249  # X coordinate Bohr
+        Y[i]  = result[i][1]*0.529177249  # Y coordinate Bohr
+        Z[i]  = result[i][2]*0.529177249  # Z coordinate Bohr
+        Q[i]  = result[i][3]  # Charge
+        print >> f1, (" %.10f     %.10f     %.10f     %.10f " %(X[i],Y[i],Z[i],Q[i]))
+    #
+    print ("  ****************************************************************** ")
+    print (" ")
+    print ("      THE COORDINATE CONVERSION HAS BEEN COMPLETED SUCCESFULLY ")
+    print (" ")
+    print ("  ****************************************************************** ")
+
+
+# ----------------------------------------------------
+# Main
+if __name__ == '__main__':
+    
+    # Get Input
+    if len(sys.argv[1:]) == 1:
+        filename = sys.argv[1]
+        convert(filename)
+    else:
+        print 'ERROR: Wrong input'
+        print 'Usage:'
+        print 'python bohr2ans.py NAME.txt'
+
